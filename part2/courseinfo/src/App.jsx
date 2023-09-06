771be1b98facc82a1a503db9984a9b2646d3656e
Virakti <viraktijain01@gmail.com>
2023-09-06 21:48:55 +0530

    Added part2 exercises

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 
+import Course from './Course'
 
 const App = () => {
   const courses = [
@@ -50,56 +51,14 @@ const App = () => {
   return <CourseList courses={courses} />
 
 }
-const Course = ({ course }) => {
 
-  return <div key = {course.id}>
-    <Header course={course}></Header>
-    <Content course={course}></Content>
-    <Total course={course}></Total>
+const CourseList = ({ courses }) => {
 
-  </div>
+  return <div>{courses.map(course => <Course key={course.id} course={course}></Course>)}</div>
 
 }
-const CourseList = ({courses}) =>
-{
-
- return <div>{courses.map(course=><Course key={course.id} course={course}></Course>)}</div>
-
-}
-const Header = (props) => {
-  console.log("Props in header is ", props.course.name)
-  return <h1>{props.course.name}</h1>
 
 
-}
-const Content = ({ course }) => {
-
-  const parts = course.parts
-  console.log("Parts obj is ", parts)
-  return <ul>
-    {
-
-      parts.map(p => {
-       return <Part key = {p.id} part={p}></Part>
-      })
 
 
-    }
-
-  </ul>
-
-}
-const Part = (props) => {
-   console.log("In part, props is ", props)
-  const part = props.part
-
-  return <li>{part.name} {part.exercises}</li>
-}
-
-const Total = ({ course }) => {
-  const parts = course.parts
-  const total = parts.reduce((total, p) => total + p.exercises, 0)
-  return <p><b>total of {total} exercises</b></p>
-
-}
 export default App
