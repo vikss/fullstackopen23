e7346af6c281670c5a165318d8699d6422dacdd0
Virakti <viraktijain01@gmail.com>
2023-09-22 20:43:48 +0530

    Phonebook exercise

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,148 @@
+import { useState, useEffect } from 'react'
+import phoneService from './services/phone'
+import './index.css'
+
+const App = () => {
+  const DEFAULT_CLASS = "default"
+  const DEFAULT_MESSAGE = { name: "", classname: DEFAULT_CLASS }
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [message, setMessage] = useState(DEFAULT_MESSAGE)
+
+  useEffect(() => {
+    console.log("here")
+    phoneService.getAll().then(res => setPersons(res))
+
+
+  }, [])
+
+
+
+  const addEntry = (event) => {
+    event.preventDefault()
+
+    let found = persons.find(person => person.name === newName)
+    if (found) {
+      if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+
+        let newEntry = { ...found, number: newNumber }
+        phoneService.editEntry(found.id, newEntry).then(res =>
+          console.log(res))
+        let modifiedArray = persons.map(p => {
+          if (p.id === found.id) {
+
+            p = newEntry
+          }
+
+          return p
+
+        })
+
+        setPersons(modifiedArray)
+        setMessage({ name: `User info of ${found.name} edited`, classname: "info" })
+        setTimeout(() => { setMessage(DEFAULT_MESSAGE) }, 5000)
+
+      }
+      else {
+
+        console.log("User cancelled the edit operation, doing nothing.")
+      }
+
+    }
+    else {
+      let newObj = { name: newName, number: newNumber }
+      let obj = persons.concat(newObj)
+      phoneService.addEntry(newObj).then(res => console.log(res))
+      setPersons(obj)
+      setMessage({ name: `Added ${newObj.name}`, classname: "info" })
+      setTimeout(() => { setMessage(DEFAULT_MESSAGE) }, 5000)
+
+    }
+    setNewName('')
+    setNewNumber('')
+
+  }
+  const deleteEntry = (event) => {
+    event.preventDefault()
+    let name = event.target.value
+    if (window.confirm(`Delete ${name}?`)) {
+      console.log("Inside the delete entry handler")
+      console.log(`Deleting the person's entry with name ${name}`)
+      let person = persons.find(p => p.name === name)
+      let updatedPersons = persons.filter(p => p.name !== name)
+
+      phoneService.deleteEntry(person.id).then(res => {
+        if (res.status === 200) {
+          console.log(res)
+          console.log("Deleted the entry")
+          setPersons(updatedPersons)
+
+        }
+        else if (res.status === 404) {
+
+          console.log("Deletion operation failed")
+          setMessage({ name: `Information of ${name} has already been removed from the server`, classname: "error" })
+          setTimeout(() => { setMessage(DEFAULT_MESSAGE) }, 5000)
+
+        }
+        else {
+
+          console.log("Something went wrong")
+        }
+
+
+      })
+    }
+    else {
+      console.log("Not deleting the entry")
+
+    }
+
+
+
+  }
+  const addName = (event) => {
+
+    console.log(event.target.value)
+    setNewName(event.target.value)
+
+  }
+  const addNumber = (event) => {
+
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Notification message={message.name} classname={message.classname}></Notification>
+      <div>filter shown with <input ></input></div>
+      <h2>add a new</h2>
+      <form onSubmit={addEntry}>
+        <div>
+          name: <input value={newName} onChange={addName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={addNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>{persons.map(person => <div>{person.name} {person.number} <button onClick={deleteEntry} value={person.name}>delete</button></div>)}</div>
+    </div>
+  )
+}
+
+const Notification = ({ message, classname }) => {
+
+  if (message == null)
+    return null
+  else if (classname == "info" || classname == "error")
+    return <div className={classname}>{message}</div>
+
+}
+
+export default App
---
+++ part2/phonebook/src/services/phone.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3000/persons'
+
+const getAll = () => {
+   const request = axios.get(baseUrl)
+   return request.then(response => {
+      console.log(response.data)
+      return response.data
+
+   })
+}
+
+const addEntry = (obj) => {
+
+   return axios.post(baseUrl, obj).then(res => {
+
+      console.log(res)
+      return res.data
+
+   })
+
+}
+
+const editEntry = (id, obj) => {
+
+   return axios.put(`${baseUrl}/${id}`, obj).then(res => {
+
+      console.log(res)
+      return res
+
+   })
+
+
+}
+const deleteEntry = (id) => {
+
+   return axios.delete(`${baseUrl}/${id}`).then(res => {
+      console.log(res)
+      return res
+
+   }
+
+   ).catch(error => {
+   console.log(error)
+     return error.response
+})
+
+}
+
+
+
+export default { getAll, addEntry, deleteEntry, editEntry }
